19918548cedbf48169d5356ec4870b2ece43f413
Khair Uddin <ku617754@gmail.com>
2023-10-29 09:58:51 +0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("Coba belajar VB.Net")
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        MsgBox("Tanda Seru", MsgBoxStyle.Exclamation, "Baca Pesan")
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        MsgBox("Tanda I ", MsgBoxStyle.Information, "Baca Pesan")
+    End Sub
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        MsgBox("Error", MsgBoxStyle.Critical, "Baca Penan")
+    End Sub
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim pesan As String
+        pesan = MsgBox("Keluar dari Program??", MsgBoxStyle.Question + MsgBoxStyle.OkCancel, "Exit")
+        If pesan = vbOK Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        End
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Label1.Text = "percobaan VB.Net"
+        Button5.Text = "Ok..!!"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Label1.Text = ""
+    End Sub
+End Class
